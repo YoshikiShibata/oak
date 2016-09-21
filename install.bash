3aa50ae988ddbc21274a1bab16c33a1608a20bcd
Yoshiki Shibata <yshibata@ca2.so-net.ne.jp>
2016-09-21 15:13:07 +0900

    added

---
+++ install.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+go build -o jo
+if [ $? != 0 ]
+then 
+    exit
+fi
+
+cp jo ~/bin
+
+TOP_DIR=$PWD
+
+# Execute tests
+for dir in simpleRun;
+do
+    cd $TOP_DIR/tests/$dir
+    ./run.bash
+done;
+
+# Clean up .class files
+cd $TOP_DIR
+find tests -name "*.class" | xargs rm
